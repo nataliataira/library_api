f75ee06587889db2f48a98567bf66c698579deee
Natalia Taira <nataliagtaira@gmail.com>
2025-05-09 12:52:57 -0300

    feat(migrations): adiciona migrations do banco h2

---
+++ src/test/resources/db/migration-h2/V3__create_loan_table.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IF NOT EXISTS loan (
+    id          BIGINT  NOT NULL AUTO_INCREMENT,
+    book_id     BIGINT,
+    reader_id   BIGINT,
+    return_date TIMESTAMP NULL,
+    created_at  TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (id),
+    FOREIGN KEY (reader_id) REFERENCES reader (id),
+    FOREIGN KEY (book_id) REFERENCES book (id)
+);
